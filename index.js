1ecd77967dc225f8cb8de6bb01a91507c4e1c6c1
AJALA MAYOWA FELIX <81765322+amfstacks@users.noreply.github.com>
2022-10-22 06:38:33 +0000

    donewithgameguess

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('hi');
+// console.log('hi');
+let maxrange = 2;
+let gainedPoint = 0;
+const userName = prompt(' Enter Your username ');
+alert("Welcome  " + userName + "  to my Guess game\n\n");
+guessNumberGame();
+function guessNumberGame() {
+
+
+  let generatedNumber = Math.floor(Math.random() * maxrange + 1);
+  
+  let guessedNumber = parseInt(prompt('Guess a number from 1 to ' + maxrange + ' :'));
+
+
+
+  if (guessedNumber == generatedNumber) {
+    gainedPoint++;
+    console.log('You guessed the correct number, you have gained ' + gainedPoint + ' point ');
+    alert('You have moved to a higher level\n\n')
+    maxrange++;
+
+    guessNumberGame();
+  }
+  else {
+    console.log('You missed this, Your current point is ' + gainedPoint + '\n Please try again\n\n');
+    guessNumberGame();
+  }  alert(maxrange);
+}
